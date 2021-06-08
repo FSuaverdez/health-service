378d4b6d0e5420a1c337fcdcf6c4dad9c8e4bb19
FSuaverdez <sfrannz@gmail.com>
2021-06-08 14:07:40 +0800

    update web clinic

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -56,8 +56,8 @@ module.exports.submit_post = async (req, res) => {
 module.exports.ticket_get = async (req, res) => {
   try {
     const ticket = await Ticket.findById(req.params.id)
-
-    res.render('pages/ticket', { ticket, rmWhitespace: true })
+    const sender = await User.findById(ticket.userId)
+    res.render('pages/ticket', { ticket, sender, rmWhitespace: true })
   } catch (err) {
     const errors = handleErrors(err)
     res.status(400).json({ errors })
